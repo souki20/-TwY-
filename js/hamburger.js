97892d8307b80bb8a96fb71d328bf4a04519584f
souki20 <souki0201@icloud.com>
2021-12-21 15:43:16 +0900

    最初のコミット

---
+++ js/hamburger.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  function navmenu() {
+
+  }
+
+  function hamburger() {
+
+  }
+
+  $("#btn").on('click', function() {
+    $(".musk").toggleClass("hmb-musk");
+    // $("#musk").addClass("hmb-musk");
+  })
+
+})
